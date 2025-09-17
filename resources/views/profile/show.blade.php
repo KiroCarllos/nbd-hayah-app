c70e8412370c10a654c80a6c5767640a94ddf4e5
Kirellos Shehata <58255421+KiroCarllos@users.noreply.github.com>
2025-09-17 23:42:59 +0300

    updates

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'الملف الشخصي - نبض الحياة')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
             <div class="col-md-4">
                 <!-- Profile Card -->
