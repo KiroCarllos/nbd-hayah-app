33ee2ed845b501dd73af0519cb164f8995bb669e
Kirellos Shehata <58255421+KiroCarllos@users.noreply.github.com>
2025-09-17 22:42:46 +0300

    updates

--- resources/views/campaigns/show.blade.php
+++ resources/views/campaigns/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', $campaign->title . ' - نبض الحياة')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
             <div class="col-md-8">
                 <!-- Campaign Images -->
